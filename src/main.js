4baf6b835b05b2083cbe00f834d9e558a4559e62
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-26 09:56:24 +0700

    Delete Library Ant design

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import './styles/style.css'
-import 'ant-design-vue/dist/reset.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Antd from 'ant-design-vue'
 
 const app = createApp(App)
-app.use(Antd)
+
 app.use(router)
 
 app.mount('#app')
