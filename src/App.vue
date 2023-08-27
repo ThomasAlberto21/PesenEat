0af350a6a62261aefe515e5df004406d9e64b482
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-27 12:20:26 +0700

    Footer hampir selesai

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar/Navbar.vue'
+import Footer from './components/Footer/Footer.vue'
 </script>
 
 <template>
-  <div>
-    <Navbar />
-  </div>
+  <Navbar />
+
   <RouterView />
+  <Footer />
 </template>
