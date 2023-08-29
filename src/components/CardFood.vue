d1fc6cc8f5aba9be23230807b86b6173907a1188
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-29 21:28:42 +0700

    Create CardFood

---
+++ src/components/CardFood.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { Rating } from 'flowbite-vue'
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <img :src="item.image" alt="Service Image" class="w-screen mx-auto mb-4 rounded-t-2xl" />
+    <div class="px-4 mt-3 mb-4">
+      <h1 class="mb-2 font-semibold tracking-tight text-black text-md md:text-2xl xl:text-2xl">
+        {{ item.title }}
+      </h1>
+
+      <div class="flex items-center justify-between">
+        <h2 class="text-xl font-medium text-gray-500">${{ item.price }}</h2>
+
+        <Rating :rating="1" :scale="1" review-link="#">
+          <template #besideText>
+            <h2 class="ml-2 text-lg font-bold text-gray-600">
+              {{ item.rating }}
+            </h2>
+          </template>
+        </Rating>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardFoodComponent'
+}
+</script>
--- src/views/Home/Food/Food.vue
+++ src/views/Home/Food/Food.vue
@@ -1,6 +1,7 @@
 <script setup>
-import CardFood from './CardFood.vue'
 import Button from '@/components/Button.vue'
+import CardFood from '@/components/CardFood.vue'
+import { foodMenu } from '@/constants/constants'
 </script>
 
 <template>
@@ -8,14 +9,23 @@ import Button from '@/components/Button.vue'
     <p class="text-xl font-semibold text-yellow-400">FOODS</p>
     <h1 class="mb-5 text-4xl font-bold xl:text-7xl">Recomendations</h1>
 
-    <CardFood />
+    <div class="grid grid-cols-2 gap-3 md:grid-cols-2 xl:grid-cols-4">
+      <div
+        v-for="item in foodMenu"
+        :key="item.id"
+        class="mb-3 bg-white border border-gray-200 shadow-xl max-w-screen rounded-2xl"
+      >
+        <CardFood :item="item" />
+      </div>
+    </div>
     <div class="text-center">
       <Button icon="fluent:food-16-filled" title="See All Foods" to="/food-menu" />
     </div>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'FoodComponent'
+  name: 'FoodParent'
 }
 </script>
