97f02fc22976cacc5cf56eaf94b8d64b793af609
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-29 21:37:25 +0700

    Setting Hero

--- src/views/Home/Hero/Hero.vue
+++ src/views/Home/Hero/Hero.vue
@@ -3,12 +3,18 @@ import Button from '@/components/Button.vue'
 </script>
 
 <template>
-  <section>
+  <section
+    class="bg-center bg-no-repeat bg-cover"
+    style="background-image: url('/images/Hero.png')"
+  >
     <div
       class="grid max-w-screen-xl h-[100vh] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 mb-20"
     >
       <div class="mr-auto place-self-center lg:col-span-8">
-        <h1 class="max-w-3xl mb-4 text-3xl font-bold leading-10 text-white md:text-7xl xl:text-7xl">
+        <h1
+          class="max-w-3xl mb-4 text-3xl font-bold leading-10 text-white md:text-7xl xl:text-7xl"
+          style="font-family: Poppins, sans-serif"
+        >
           Get your <b class="text-yellow-400">favorite food </b> straight to your doorstep easily
           and quickly
         </h1>
@@ -17,20 +23,9 @@ import Button from '@/components/Button.vue'
     </div>
   </section>
 </template>
+
 <script>
 export default {
   name: 'HeroComponent'
 }
 </script>
-
-<style>
-section {
-  background-image: url('/images/Hero.png');
-  background-size: cover;
-  background-position: center;
-}
-
-h1 {
-  font-family: 'Poppins', sans-serif;
-}
-</style>
